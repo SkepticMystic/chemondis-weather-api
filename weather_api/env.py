b84a827a96bfc5545b9bd8735263ae6c9865efb5
SkepticMystic <rossk29@gmail.com>
2024-01-28 21:46:17 +0100

    CACHE_TTL_MINS is one of enum, not entirely variable

--- weather_api/env.py
+++ weather_api/env.py
@@ -1,8 +1,10 @@
 from dotenv import get_key
 
 CACHE_TTL_MINS = get_key('.env', 'CACHE_TTL_MINS')
-# TODO: I believe the value should be in an enum (e.g. 5, 10, 15, 30, 60)
-if (CACHE_TTL_MINS is None or CACHE_TTL_MINS == ''):
+if (
+    CACHE_TTL_MINS is None or
+    CACHE_TTL_MINS not in ['5', '10', '60']
+):
     CACHE_TTL_MINS = 5
 else:
     CACHE_TTL_MINS = int(CACHE_TTL_MINS)
